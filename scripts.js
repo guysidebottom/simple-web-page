3dfc21df88e994041449c76a89f59a06f1ce8f6a
Guy Sidebottom <guy.sidebottom@digital.homeoffice.gov.uk>
2018-03-26 14:28:41 +0100

    Added button with event listener

--- scripts.js
+++ scripts.js
@@ -1,1 +1,12 @@
-window.alert(5+6);
+// create the button
+var button = document.createElement("button");
+button.innerHTML = "Do something";
+
+// append the button somewhere
+var body = document.getElementsByTagName("body")[0];
+body.appendChild(button);
+
+// event handler
+button.addEventListener("click", function() {
+    alert("Did something");
+})
